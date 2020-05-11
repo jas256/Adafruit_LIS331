5c6a3c662ccb8b55c4ff4d09326d4137fa6c7f58
siddacious <nospam187+github@gmail.com>
2020-05-11 10:26:06 -0700

    updated filter example

--- examples/lis331_filter_demo/lis331_filter_demo.ino
+++ examples/lis331_filter_demo/lis331_filter_demo.ino
@@ -1,5 +1,9 @@
 
-// Basic demo for accelerometer readings from Adafruit H3LIS331
+/* Demo of the filter methods for the Adafruit H3LIS331 and LIS331HH sensor breakouts
+Prints plotter formatted  measurements before and after having the filters applied. Multiple options are available, comment out and uncomment different options and re-upload the sketch to see their effect.
+
+**Note** The higher the range, the more noise the sinal will have. Additionally the H3LIS331DL is _especially_ noisy.
+*/
 
 #include <Wire.h>
 #include <SPI.h>
@@ -7,15 +11,10 @@
 #include <Adafruit_LIS331HH.h>
 #include <Adafruit_Sensor.h>
 
-// Used for software SPI
-#define LIS331_SCK 13
-#define LIS331_MISO 12
-#define LIS331_MOSI 11
-// Used for hardware & software SPI
-#define LIS331_CS 10
 unsigned long time;
 unsigned long last_time;
 
+bool waiting_to_reset;
 //Adafruit_H3LIS331 lis = Adafruit_H3LIS331();
 Adafruit_LIS331HH lis = Adafruit_LIS331HH();
 
@@ -23,55 +22,45 @@ void setup(void) {
   Serial.begin(115200);
   while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
 
-//  Serial.println("H3LIS331 test!");
-
-//  if (!lis.begin_SPI(LIS331_CS)) {
-//  if (!lis.begin_SPI(LIS331_CS, LIS331_SCK, LIS331_MISO, LIS331_MOSI)) {
  if (! lis.begin_I2C()) {   // change this to 0x19 for alternative i2c address
     Serial.println("Couldnt start");
     while (1) yield();
   }
-  lis.setRange(LIS331HH_RANGE_6_G);
-//  lis.setLPFCutoff(LIS3X_LPF_37_HZ); // requires that DataRate is a LOW_POWER rate
-  lis.setDataRate(LIS331_DATARATE_50_HZ);
-//  lis.setHPFReference(127);
-//  ,-48.67
-//  ,-48.82
-//  ,-48.82
-//This register sets the acceleration value taken as a reference for the high-pass filter output.
-//When filter is turned onand HPM bits are set to “01”, filter out is generated taking this value as a reference
-//  lis.setHPFReference(255);
-//  ,10.17
-//  ,9.96
-//  ,10.02
-//  ,9.96
-//  ,10.02
-//  ,10.11
-//  HPFReset();
-  lis.setHPFReference(0);
+  // lis.setRange(H3LIS331_RANGE_100_G); // For the H3LIS331
+  lis.setRange(LIS331HH_RANGE_6_G); // For the LIS331HH
 
+  // lis.setDataRate(LIS331_DATARATE_LOWPOWER_10_HZ); // Use to test the low-pass filter
+  lis.setDataRate(LIS331_DATARATE_1000_HZ); // Use the highest data rate for a higher resolution signal
 
-  lis.enableHighPassFilter(true, LIS3X_HPF_0_0025_ODR, true); //0.125Hz Cutoff
+  // lis.setLPFCutoff(LIS331_LPF_37_HZ); // requires that DataRate is a LOW_POWER rate
+  lis.enableHighPassFilter(true, LIS331_HPF_0_0025_ODR); //0.125Hz Cutoff
+
+//  lis.enableHighPassFilter(true, LIS331_HPF_0_0025_ODR, true); // use the reference
+//  lis.setHPFReference(127); // value is signed from -127 to 127 for negative and positive offsets
 //  Serial.print("HPF reference: ");Serial.println(lis.getHPFReference());
 
-  
   time = micros();
   last_time = time;
-//    1000 / 100 => 10.000000
+  waiting_to_reset = false;
 }
 
 void loop() {
-  /* Get a new sensor event, normalized */
-  sensors_event_t event;
-  lis.getEvent(&event);
-  time = micros();
+  measure_n_times(255);
+  measure_n_times(255);
+  measure_n_times(255);
+  measure_n_times(255);
+  // lis.HPFReset(); // instantly zeros the mesurements, avoiding the "warm up" time of the High-pass filter
+  measure_n_times(255);
+  lis.enableHighPassFilter(false);
+  measure_n_times(255);
+  lis.enableHighPassFilter(true, LIS331_HPF_0_0025_ODR);
+}
 
-  /* Display the results (acceleration is measured in m/s^2) */
-//  Serial.print(event.acceleration.x);
-//  Serial.print(","); Serial.print(event.acceleration.y);
-  Serial.print(","); Serial.print(event.acceleration.z);
-//  Serial.print(","); Serial.print((time-last_time)/1000);
-  Serial.println();
-  last_time=time;
-  delayMicroseconds(1000);
+void measure_n_times(uint8_t count){
+  for(int i=0; i< count; i++){
+    sensors_event_t event;
+    lis.getEvent(&event);
+    Serial.println(event.acceleration.z);
+    delayMicroseconds(1000);
+  }
 }
